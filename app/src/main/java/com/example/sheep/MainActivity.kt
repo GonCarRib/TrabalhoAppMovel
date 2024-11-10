652b9fcb5227a9dc2364b523ea95cf1cc5b2612b
a043540 <a043540@umaia.pt>
2024-11-10 18:09:16 +0000

    versao.2

--- app/src/main/java/com/example/sheep/MainActivity.kt
+++ app/src/main/java/com/example/sheep/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,7 +22,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.sheep.ui.theme.SheepTheme
@@ -36,13 +36,15 @@ class MainActivity : ComponentActivity() {
                 LazyColumn(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .background(color = MaterialTheme.colorScheme.backgroundColor),
-                    contentPadding = PaddingValues(10.dp)
+                        .background(color = MaterialTheme.colorScheme.backgroundColor)
+                        .padding(top = 65.dp)
+                        .padding(10.dp)
                 ) {
                     items(20) {
-                        botaoDeal()
+                        BotaoDeal()
                     }
                 }
+                InfoDeal()
             }
         }
     }
@@ -50,7 +52,7 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun botaoDeal() {
+fun BotaoDeal() {
     Button(
         colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.backgroundBotaoColor),
         onClick = { onClick() },
@@ -62,7 +64,6 @@ fun botaoDeal() {
     {
         Row(
             modifier = Modifier
-                .align(alignment = Alignment.CenterVertically)
                 .fillMaxWidth()
         )
         {
@@ -72,28 +73,78 @@ fun botaoDeal() {
                 contentDescription = null
             )
             Spacer(
-                modifier = Modifier.width(15.dp)
+                modifier = Modifier
+                    .width(15.dp)
             )
             Text(
                 "Buy Batman: Arkham Knight",
                 color = Color.White,
-                modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically)
 
             )
             Spacer(
-                modifier = Modifier.width(45.dp)
+                modifier = Modifier
+                    .width(45.dp)
             )
             Text(
                 "20.00€",
                 color = Color.White,
-                modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically)
             )
         }
     }
 }
 
 @Composable
-fun infoDeal() {
+fun InfoDeal() {
+    Box(
+        modifier = Modifier
+            .padding(start = 15.dp, end = 15.dp, top = 15.dp)
+            .height(50.dp)
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.backgroundBotaoColor)
+            .padding(start = 15.dp , end = 5.dp)
+
+    )
+    {
+        Row(
+        )
+        {
+            Text(
+                "Loja",
+                color = Color.White,
+                modifier = Modifier
+                    .fillMaxHeight()
+
+
+            )
+            Spacer(
+                modifier = Modifier
+                    .width(15.dp)
+            )
+            Text(
+                "Jogo",
+                color = Color.White,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .align(alignment = Alignment.CenterVertically)
+            )
+            Spacer(
+                modifier = Modifier
+                    .width(150.dp)
+            )
+            Text(
+                "Preço",
+                color = Color.White,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .align(alignment = Alignment.CenterVertically)
+            )
+        }
+
+    }
 
 }
 
